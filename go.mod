d997b9eac929ecb2cb59c5b453bdd852ca3d930f
udajne-jan <rehanekjan@gmail.com>
2021-08-02 16:18:28 +0200

    connection to database tested

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sql_to_cups_interface
 
 go 1.16
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	dbUrl := getSqlUrl()
 
-	myEnv := importEnv()
+	// Connect to the database
+	conn, err := pgx.Connect(context.Background(), dbUrl)
+	if err != nil {
+		log.Fatal("Error connecting to DB:", err)
+	}
 
-	fmt.Println(myEnv)
+	defer conn.Close(context.Background())
+
+	// Query database
+	rows, err := conn.Query(context.Background(), "select name from printers_temp")
+	if err != nil {
+		log.Fatal("Error querying DB:", err)
+	}
+	defer rows.Close()
+
+	// Iterate over returned rows
+	for rows.Next() {
+		var s string
+
+		err := rows.Scan(&s)
+		if err != nil {
+			log.Fatal("Error reading rows:", err)
+		}
+
+		fmt.Println(s)
+	}
+
 }
 
 // Variables needed to connect to the database are stored in a .env file
@@ -24,4 +51,13 @@ func importEnv() map[string]string {
 	}
 
 	return myEnv
+}
+
+// Provides valid url for connecting to the database based on env variables
+func getSqlUrl() string {
+	myEnv := importEnv()
+
+	url := myEnv["SQL_PREFIX"] + myEnv["SQL_USER"] + ":" + myEnv["SQL_PASSWORD"] + "@" + myEnv["SQL_IP"] + ":" + myEnv["SQL_PORT"] + "/" + myEnv["SQL_DATABASE"]
+
+	return url
 }
